1fdaab6f3da2eca0d80168a1f141bd19f47793c9
manuel alfaro <manuel.alfaro247@gmail.com>
2025-04-19 13:43:56 -0500

    Debugging Lab

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,16 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger; // Use this to pause and inspect variables
+
+    for (let i = 0; i < quantity; i++) { // Fixed: i = 0, not 1
         totalPrice += pricePerItem;
     }
 
     if (quantity >= 10) {
-        totalPrice *= 0.9;
+        totalPrice *= 0.9; // 10% discount
     }
 
     return totalPrice;
 }
 
-
-
-module.exports = calculateDiscountedPrice;
+module.exports = calculateDiscountedPrice
